5f903ce79652d9171855774de6d941d741bf1292
pulatov03 <pulatovubaydulla@gmail.com>
2021-09-01 14:30:06 +0500

    update

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def cal():
+    x = 1
+    y = 2
+    return x + y
 
 # Create your views here.
+def say_hello(request):
+    x = cal()
+    return render(request, 'hello.html', {'number': x})
